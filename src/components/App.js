50e4bc2d75c6912962656bd02e6436ee043f5bfa
Arrian Purcell <arrian.purcell@gmail.com>
2017-12-03 19:22:06 -0800

    Updating personal image. Improving fonts and styling.

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,23 @@ import ScrollableAnchor from 'react-scrollable-anchor'
 import { Parallax } from 'react-parallax';
 
 const Chevron = (props) => (
-	<a href={'#' + props.href} style={{ textAlign: 'bottom', marginBottom: 'auto', marginTop: 'auto', width: '100%', textAlign: 'center' }}><Icon size='big' inverted name='chevron down' /></a>
+	<a href={'#' + props.href} style={{ textAlign: 'bottom', marginBottom: 'auto', marginTop: 'auto', width: '100%', textAlign: 'center' }}><Icon link size='big' inverted name='chevron down' /></a>
 );
 
 const Title = (props) => (
 	<div style={{ display: 'flex', flexDirection: 'column', background: 'url(./images/header.jpg)', backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', minHeight: '100%', width: '100%', textAlign: 'center', paddingTop: 70 }}>
-		<Image src='./images/me.jpg' size='medium' shape='circular' centered />
-		<h1 style={{ fontSize: '8vw', color: 'white', backgroundColor: '#2c2d22' }}>Arrian Purcell</h1>
+		<Image src='./images/me.jpg' size='medium' circular centered />
+		<h1 style={{ fontSize: '4em', color: 'white', backgroundColor: '#2c2d22' }}>Ari Purcell</h1>
 		<div>
-			<Label>Full Stack Software Engineer</Label>
-			<a href="https://www.google.com/maps/place/Vancouver,+BC,+Canada"><Label><Icon name='marker' /> Vancouver - Canada</Label></a>
+			<Label>Software Engineer</Label>&nbsp;
+			<Label as='a' href="https://www.google.com/maps/place/Vancouver,+BC,+Canada"><Icon name='marker' /> Vancouver - Canada</Label>
 		</div>
 
 		<div style={{ marginTop: 10 }}>
-			<a href="http://github.com/arrian"><Icon size='big' inverted name='github square' /></a>
-			<a href="http://instagram.com/aripurcell/"><Icon size='big' inverted name='instagram' /></a>
-			<a href="http://linkedin.com/in/arrianpurcell"><Icon size='big' inverted name='linkedin square' /></a>
-			<a href="http://youtube.com/user/ArrianPurcell"><Icon size='big' inverted name='youtube square' /></a>
+			<a href="http://github.com/arrian"><Icon link size='big' inverted name='github square' /></a>
+			<a href="http://instagram.com/aripurcell/"><Icon link size='big' inverted name='instagram' /></a>
+			<a href="http://linkedin.com/in/arrianpurcell"><Icon link size='big' inverted name='linkedin square' /></a>
+			<a href="http://youtube.com/user/ArrianPurcell"><Icon link size='big' inverted name='youtube square' /></a>
 		</div>
 
 		<Chevron href='about' />
@@ -42,23 +42,26 @@ const About = (props) => (
 	<ScrollableAnchor id={'about'}>
 		<div style={{ display: 'flex', flexDirection: 'column', backgroundColor: '#2c2d22', color: 'white', minHeight: '100%' }}>
 			<Container text style={{ marginTop: 20 }}>
-				<Header as='h2' inverted textAlign='center'>About</Header>
+				<Header as='h2' inverted textAlign='center'><Icon name='circle user' size='mini' /> About</Header>
 				<p>I am a full stack software engineer with a particular focus on building web experiences and interfaces that excite. I love solving problems and delivering creative solutions using a variety of technologies. During a normal day you would see me using a good mix of Javascript (using both Node and React), HTML and CSS for both small and large projects.</p>
-				<p> My work experience has led me to build web applications for the health care sector and my education has led me to research the impact of interfaces on user experiences. I hope to promote designing for usability.</p>
+				<p> My work experience has led me to build web applications for the e-commerce and health care sectors and my education has led me to research the impact of interfaces on user experiences. I hope to promote designing for usability.</p>
 
-				<Header as='h3' inverted textAlign='center'><Icon name='wrench' />Skills</Header>
+				<Header as='h3' inverted textAlign='center'>Skills</Header>
+
+				<Divider inverted />
 
 				<Table basic='very' celled inverted>
 					<Table.Body>
 						<Skill name='Languages' labels={['javascript', 'html', 'css', 'java', 'python', 'c++']}/>
 						<Skill name='Platforms' labels={['macos', 'windows', 'linux', 'android', 'node']}/>
-						<Skill name='Libraries' labels={['react', 'redux', 'angular', 'jquery', 'd3.js', 'mocha', 'selenium']}/>
+						<Skill name='Libraries' labels={['react', 'redux', 'angular', 'vue.js', 'jquery', 'd3.js', 'mocha', 'selenium']}/>
 						<Skill name='Tools' labels={['git', 'docker', 'bamboo', 'jira', 'jenkins', 'vagrant', 'gradle', 'illustrator']}/>
 					</Table.Body>
 				</Table>
 
 			</Container>
 			<Chevron href='projects' />
+			<br />
 		</div>
 	</ScrollableAnchor>
 );
@@ -70,7 +73,7 @@ const ProjectHeader = (props) => (
 const Project = (props) => (
 		<Grid.Row color={props.color}>
 			<Grid.Column width={3}>
-				<a href={props.link}><Image src={props.image} shape='circular' style={{ backgroundColor: 'rgb(44, 45, 34)' }} /></a>
+				<a href={props.link}><Image src={props.image} size='medium' style={{ backgroundColor: 'rgb(210, 206, 203)', border: '2px solid black' }} circular /></a>
 			</Grid.Column>
 			<Grid.Column width={13}>
 				{<ProjectHeader title={props.title} link={props.link} />}
@@ -96,13 +99,14 @@ const Projects = (props) => (
 					<Project title='Social Landscape' labels={['mobile application', 'android', 'java']} description='As part of a university project, ported the "Social Landscape" iOS mobile app to the Android platform.' image='/images/project-07.svg' />
 					<Project title='FHIR Validator' labels={['web application', 'javascript', 'fhir']} description='Implemented a Javascript validator for validating that resources conform to the HL7 FHIR specification.' image='/images/project-08.svg' link='https://github.com/arrian/fhir-validator-js' />
 				</Grid>
-
+				<br />
+				<br />
 			</Container>
 		</div>
 	</ScrollableAnchor>
 );
 
-class SidebarLeftPush extends Component {
+class App extends Component {
   state = { visible: false }
 
   toggleVisibility = () => this.setState({ visible: !this.state.visible })
@@ -121,4 +125,4 @@ class SidebarLeftPush extends Component {
   }
 }
 
-export default SidebarLeftPush
+export default App
